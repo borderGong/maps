a9af81be05094e9acd2268a26d22c5f4e99afe72
border <border.gong@gmail.com>
2018-04-26 21:15:41 +0800

    fix: 分离数据 添加加载效果

--- app.js
+++ app.js
@@ -1,47 +1,10 @@
+const load = $(`<span>loading...</span>`);
 // 地图加载错误信息
 function mapErrorHandler(){
     document.querySelector('#map').innerHTML = '地图加载错误，请刷新重试';
 }
 // 定义map
 let map, markers, infowindow;
-const points = [
-    {
-        title: '故宫博物院',
-        lat: 39.9176718710,
-        lng: 116.3971864841,
-        pinyin: 'gugong'
-    },
-    {
-        title: '景山公园',
-        lat: 39.9264914074,
-        lng: 116.3965786593,
-        pinyin: 'jingshan'
-    },
-    {
-        title: '北海公园',
-        lat: 39.9272692029,
-        lng: 116.3886708770,
-        pinyin: 'beihaigongyuan',
-    },
-    {
-        title: '国家大剧院',
-        lat: 39.9048340787,
-        lng: 116.3897190553,
-        pinyin: 'guojiadajuyuan'
-    },
-    {
-        title: '中山公园',
-        lat: 39.9117461163,
-        lng: 116.3948672466,
-        pinyin: 'zhongshangongyuan',
-    },
-    {
-        title: '西单大悦城',
-        lat: 39.9105338820,
-        lng: 116.3732057966,
-        pinyin: 'xidandayuecheng',
-    }
-];
 function initMap(){
     // 初始化地图
     map = new google.maps.Map(document.getElementById('map'), {
@@ -62,7 +25,9 @@ function initMap(){
     markers.forEach(item => {
         item.addListener('click', function() {
             const pinyin = points.filter(point => point.title === item.title)[0].pinyin;
-                // .then(([response1, response2]) => [response1.json(), response2])
+            // loading
+            infowindow.setContent(load.get(0));
+            infowindow.open(map, item);
             fetchApiInfo(item.position.lat(), item.position.lng(), pinyin)
                 .then(([res1, res2]) => {
                     console.log(res1, res2);
@@ -121,6 +86,8 @@ function fetchApiInfo(lat, lng, pinyinName){
         this.clickItem = function(e){
             const index = points.findIndex(item => item.title === e.title);
             const item = markers[index];
+            infowindow.setContent(load.get(0));
+            infowindow.open(map, item);
             fetchApiInfo(item.position.lat(), item.position.lng(), points[index].pinyin)
                 .then(([res1, res2]) => {
                     let content;
---
+++ data.js
@@ -0,0 +1,41 @@
+/**
+ * @description 数据源
+ */
+const points = [
+    {
+        title: '故宫博物院',
+        lat: 39.9176718710,
+        lng: 116.3971864841,
+        pinyin: 'gugong'
+    },
+    {
+        title: '景山公园',
+        lat: 39.9264914074,
+        lng: 116.3965786593,
+        pinyin: 'jingshan'
+    },
+    {
+        title: '北海公园',
+        lat: 39.9272692029,
+        lng: 116.3886708770,
+        pinyin: 'beihaigongyuan',
+    },
+    {
+        title: '国家大剧院',
+        lat: 39.9048340787,
+        lng: 116.3897190553,
+        pinyin: 'guojiadajuyuan'
+    },
+    {
+        title: '中山公园',
+        lat: 39.9117461163,
+        lng: 116.3948672466,
+        pinyin: 'zhongshangongyuan',
+    },
+    {
+        title: '西单大悦城',
+        lat: 39.9105338820,
+        lng: 116.3732057966,
+        pinyin: 'xidandayuecheng',
+    }
+];
